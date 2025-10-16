c57d311e7743a52b7b2ec3b3115db328ef07e85d
Hakan Cineli <hakancineli@example.com>
2025-10-17 01:28:55 +0300

    fix: Tailwind CSS config dosyasını düzelt

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,md
